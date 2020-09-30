7b08d1bb292031abaec3cd40fef3787a58c51bfb
Laura Sora <laurasora@Lauras-MacBook-Pro.local>
2020-09-30 09:33:16 -0400

    change carousel

--- js/carousel.js
+++ js/carousel.js
@@ -1,13 +1,34 @@
+const images=[
+    {src:'_path1',
+    description:'text1'},
+    {src:'_path2',
+    description:'text2'},
+    {src:'_path3',
+    description:'text3'}
+]
+
 const carousell=document.querySelectorAll('.portfolio-carousell')
 const carousellArr=(Array.from(carousell))
 
 const btnLeft=document.querySelector('.portfolio-btn-left')
 const btnRight=document.querySelector('.portfolio-btn-right')
 const container=document.querySelector('.portfolio-carousell')
+const descriptionText=document.querySelector('.portfolio_arrow > p');
+    container.style.backgroundColor="black";
+    let start=3
+    btnLeft.addEventListener('click', function(){
 
-btnLeft.addEventListener('click',function(e){
-    container.style.backgroundColor="black"
+        descriptionText.innerHTML=images[start].description
+        
+        return start++
+    })
+    
+    
+
+
+btnRight.addEventListener('click',function(){
+    descriptionText.innerHTML=images[start-1].description
+        start=start-1
+        return start
 })
-btnRight.addEventListener('click',function(e){
-    container.style.backgroundColor="grey"
-})
+
