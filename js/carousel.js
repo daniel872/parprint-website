3da46bbd7fa4fa535356b0a015ecb6987187d881
Laura Sora <laurasora@Lauras-MacBook-Pro.local>
2020-10-02 10:55:30 -0400

    change carousel script and media query

--- js/carousel.js
+++ js/carousel.js
@@ -28,6 +28,7 @@ const btnLeft=document.querySelector('.portfolio-btn-left')
 const btnRight=document.querySelector('.portfolio-btn-right')
 const container=document.querySelector('.portfolio-carousell')
 const descriptionText=document.querySelector('.portfolio_arrow > p');
+descriptionText.textContent=images[0].description
 container.style.backgroundColor="black";
 let state={
     start:0,
@@ -35,62 +36,111 @@ let state={
     btnRightpressed:false,
     counter:0
 }
-    btnLeft.addEventListener('click', function(){
-        if(state.counter<images.length){
-        let start=(images.length-1) - state.counter
+//     btnLeft.addEventListener('click', function(){
+//         if(state.counter<images.length){
+//         let start=(images.length-1) - state.counter
+//         state.counter++
+//         state.btnLeftpressed=true
+//         descriptionText.innerHTML=images[start].description
+//         console.log(`start este ${start}
+//                         counter este ${state.counter} and ${state.btnLeftpressed}`)
+
+//         return state}
+//         else if (counter===images.length){
+//             counter=0
+//             let start=(images.length-1) - counter
+//             counter++
+//             descriptionText.innerHTML=images[start].description
+//             console.log(`start este ${start}
+//                             counter este ${counter}`)
+//             return counter
+//         }
+//     })
+    
+    
+
+
+// btnRight.addEventListener('click',function(){
+//     if(state.counter===0){
+//         start=state.counter
+//         state.counter++
+//         state.btnRightpressed=true;
+//         descriptionText.innerHTML=images[start].description
+//         console.log(`start este ${start}
+//                         counter este ${state.counter}`)
+//         return state.counter}
+
+//         else if(state.counter>0 && state.btnRightpressed ){
+//                 state.start++
+//                 state.counter++
+//                 start=state.start
+//                 state.btnRightpressed=true;
+//                 state.btnLeftpressed=false
+//                 descriptionText.innerHTML=images[start].description
+//                 console.log(`start este ${start}
+//                                 counter este ${state.counter}`)
+                
+//                 return state
+//         }
+//         else if (state.counter>=1 && state.btnLeftpressed){
+//             let start=images.length - (state.counter-1)
+//             descriptionText.innerHTML=images[start].description
+//             state.start=start
+//             state.btnLeftpressed=false
+//             state.btnRightpressed=true
+//             console.log(`start este ${start}
+//             counter este ${state.counter} and ${state.btnLeftpressed}`)
+//             return state
+//         }
+        
+// })
+
+btnLeft.addEventListener('click', function(){
+    if(state.counter>=0 && state.counter<images.length-1 && !state.btnRightpressed){
+        state.start=(images.length-1) - state.counter
+        descriptionText.textContent=images[state.start].description
         state.counter++
         state.btnLeftpressed=true
-        descriptionText.innerHTML=images[start].description
-        console.log(`start este ${start}
-                        counter este ${state.counter} and ${state.btnLeftpressed}`)
+        console.log(state)
+        return state
+    }
+    else if(state.counter===images.length-1 && !state.btnRightpressed){
+        state.counter=0
+        state.start=0
+        state.btnLeftpressed=true
+        descriptionText.textContent=images[state.start].description
+        console.log(state)
+        return state
+    }
+    else if(state.btnRightpressed){
+        state.start--
+        // state.btnRightpressed=false
+        descriptionText.textContent=images[state.start].description
+        console.log(state)
+    }
 
-        return state}
-        else if (counter===images.length){
-            counter=0
-            let start=(images.length-1) - counter
-            counter++
-            descriptionText.innerHTML=images[start].description
-            console.log(`start este ${start}
-                            counter este ${counter}`)
-            return counter
-        }
-    })
-    
-    
-
-
-btnRight.addEventListener('click',function(){
-    if(state.counter===0){
-        start=state.counter
-        state.counter++
-        state.btnRightpressed=true;
-        descriptionText.innerHTML=images[start].description
-        console.log(`start este ${start}
-                        counter este ${state.counter}`)
-        return state.counter}
-
-        else if(state.counter>0 && state.btnRightpressed ){
-                state.start++
-                start=state.start
-                state.btnRightpressed=true;
-                state.btnLeftpressed=false
-                descriptionText.innerHTML=images[start].description
-                console.log(`start este ${start}
-                                counter este ${state.counter}`)
-                
-                return state
-        }
-        else if (state.counter>=1 && state.btnLeftpressed){
-            let start=images.length - (state.counter-1)
-            descriptionText.innerHTML=images[start].description
-            state.start=start
-            state.btnLeftpressed=false
-            state.btnRightpressed=true
-            console.log(`start este ${start}
-            counter este ${state.counter} and ${state.btnLeftpressed}`)
-            return state
-        }
-        
+    else if(!state.start){
+        state.btnRightpressed=false
+    }
 })
 
+btnRight.addEventListener('click', function(){
+   if(state.counter<images.length-1 && state.start<images.length-1) {
+    state.counter++
+    state.start++
+    state.btnRightpressed=true
+    state.btnLeftpressed=false
+    descriptionText.textContent=images[state.start].description
+    console.log(state)
+   } 
+   else if(state.counter===images.length-1 || state.start===images.length-1){
+    state.counter=0
+    state.start=0
+    state.btnRightpressed=true
+    state.btnLeftpressed=false
+    descriptionText.textContent=images[state.start].description
+    console.log(state)
+   }
+
+})
 
